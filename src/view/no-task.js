3493342b7eb638efe0b57b903402aac3f6327d31
pendeldam <45753426+pendeldam@users.noreply.github.com>
2020-08-13 22:46:23 +0300

    module4-task2 completed

---
+++ src/view/no-task.js
@@ -0,0 +1,27 @@
+import {createElement} from '../utils.js';
+
+const createNoTaskMarkup = () => {
+  return `<p class="board__no-tasks">Click «ADD NEW TASK» in menu to create your first task</p>`;
+};
+
+export default class NoTask {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoTaskMarkup();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
