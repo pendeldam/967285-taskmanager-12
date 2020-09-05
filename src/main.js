8bbd3d965a44687bd6972893ef1010ad648aff53
pendeldam <45753426+pendeldam@users.noreply.github.com>
2020-09-05 17:15:30 +0300

    added statistics chart

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import TasksModel from './model/tasks.js';
 import FilterModel from './model/filter.js';
+import StatisticsView from './view/statistics.js';
 import SiteMenuView from './view/site-menu.js';
 import FilterPresenter from './presenter/filter.js';
 import BoardPresenter from './presenter/board.js';
 import {generateTask} from './mock/task.js';
-import {render, RenderPosition} from './utils/render.js';
+import {render, RenderPosition, remove} from './utils/render.js';
+import {MenuItem, UpdateType, FilterType} from './const.js';
 
 const mainEl = document.querySelector(`.main`);
 const TASK_COUNT = 20;
@@ -14,16 +16,43 @@ const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
 
 const filterModel = new FilterModel();
+const siteMenuComponent = new SiteMenuView();
 
-render(mainEl.querySelector(`.main__control`), new SiteMenuView(), RenderPosition.BEFOREEND);
+render(mainEl.querySelector(`.main__control`), siteMenuComponent, RenderPosition.BEFOREEND);
 
 const boardPresenter = new BoardPresenter(mainEl, tasksModel, filterModel);
 const filterPresenter = new FilterPresenter(mainEl, filterModel, tasksModel);
 
+const handleTaskNewFormClose = () => {
+  siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = false;
+  siteMenuComponent.setMenuItem(MenuItem.TASKS);
+};
+
+let statisticsComponent = null;
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.ADD_NEW_TASK:
+      remove(statisticsComponent);
+      boardPresenter.destroy();
+      filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
+      boardPresenter.init();
+      boardPresenter.createTask(handleTaskNewFormClose);
+      siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
+      break;
+    case MenuItem.TASKS:
+      boardPresenter.init();
+      remove(statisticsComponent);
+      break;
+    case MenuItem.STATISTICS:
+      boardPresenter.destroy();
+      statisticsComponent = new StatisticsView(tasksModel.getTasks());
+      render(mainEl, statisticsComponent, RenderPosition.BEFOREEND);
+      break;
+  }
+};
+
+siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
+
 filterPresenter.init();
 boardPresenter.init();
-
-document.querySelector(`#control__new-task`).addEventListener(`click`, (evt) => {
-  evt.preventDefault();
-  boardPresenter.createTask();
-});
--- src/presenter/board.js
+++ src/presenter/board.js
@@ -6,7 +6,7 @@ import TaskNewPresenter from "./task-new.js";
 import NoTaskView from '../view/no-task.js';
 import LoadMoreButtonView from '../view/load-more-button.js';
 import {sortTaskUp, sortTaskDown} from "../utils/task.js";
-import {SortType, UserAction, UpdateType, FilterType} from '../const.js';
+import {SortType, UserAction, UpdateType} from '../const.js';
 import {render, remove, RenderPosition} from '../utils/render.js';
 import {filter} from '../utils/filter.js';
 
@@ -34,9 +34,6 @@ export default class Board {
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
     this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
 
-    this._tasksModel.addObserver(this._handleModelEvent);
-    this._filterModel.addObserver(this._handleModelEvent);
-
     this._taskNewPresenter = new TaskNewPresenter(this._taskListComponent, this._handleViewAction);
   }
 
@@ -44,13 +41,24 @@ export default class Board {
     render(this._boardContainer, this._boardComponent, RenderPosition.BEFOREEND);
     render(this._boardComponent, this._taskListComponent, RenderPosition.BEFOREEND);
 
+    this._tasksModel.addObserver(this._handleModelEvent);
+    this._filterModel.addObserver(this._handleModelEvent);
+
     this._renderBoard();
   }
 
-  createTask() {
-    this._currentSortType = SortType.DEFAULT;
-    this._filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
-    this._taskNewPresenter.init();
+  destroy() {
+    this._clearBoard({resetRenderedTaskCount: true, resetSortType: true});
+
+    remove(this._taskListComponent);
+    remove(this._boardComponent);
+
+    this._tasksModel.removeObserver(this._handleModelEvent);
+    this._filterModel.removeObserver(this._handleModelEvent);
+  }
+
+  createTask(callback) {
+    this._taskNewPresenter.init(callback);
   }
 
   _getTasks() {
